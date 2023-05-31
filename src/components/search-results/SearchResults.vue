6fe297e2ec4e805d7792778e9d3127e5dd4f1025
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-05-31 12:32:26 -0600

    active place n move to focused place

--- src/components/search-results/SearchResults.vue
+++ src/components/search-results/SearchResults.vue
@@ -5,11 +5,16 @@
         <span>Wait please...</span>
     </div>
     <ul v-else-if="places.length > 0" class="list-group mt-3">
-        <li v-for="place in places" :key="place.id" class="list-group-item list-group-item-action">
+
+        <li v-for="place in places" :key="place.id" 
+            class="list-group-item list-group-item-action"
+            :class="{'active' : place.id === activePlace}"
+            @click="onPlaceClicked(place)">
             <h5>{{place.text}}</h5>
             <p>{{place.place_name}}</p>
             <div align="right">
-                <button class="btn btn-outline-primary btn-sm">
+                <button class="btn btn-outline-primary btn-sm" 
+                    :class="( place.id === activePlace ) ? 'btn-outline-light' : 'btn-outline-primary'">
                     Direcciones
                 </button>
             </div>
