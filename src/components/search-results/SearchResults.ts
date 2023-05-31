5d5c5d5658d49728c5e78814508ab4e689183fb5
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-05-31 11:19:29 -0600

    showPlaces in search results

--- src/components/search-results/SearchResults.ts
+++ src/components/search-results/SearchResults.ts
@@ -1,13 +1,14 @@
 import { defineComponent } from 'vue';
+import { usePlacesStore } from '@/composables';
 
 export default defineComponent({
     name: 'SearchResults',
     setup() {
 
-        
+        const { isLoadingPlaces, places } = usePlacesStore()
 
         return {
-
+            isLoadingPlaces, places
         } 
     }
 });
--- src/components/search-results/SearchResults.vue
+++ src/components/search-results/SearchResults.vue
@@ -1,27 +1,13 @@
 <script lang="ts" src="./SearchResults.ts"/>
 <template>
-    <ul class="list-group mt-3">
-        <li class="list-group-item list-group-item-action">
-            <h5>Nombre del lugar</h5>
-            <p>In adipisicing dolor magna enim ex nulla in ad amet.</p>
-            <div align="right">
-                <button class="btn btn-outline-primary btn-sm">
-                    Direcciones
-                </button>
-            </div>
-        </li>
-        <li class="list-group-item list-group-item-action">
-            <h5>Nombre del lugar</h5>
-            <p>In adipisicing dolor magna enim ex nulla in ad amet.</p>
-            <div align="right">
-                <button class="btn btn-outline-primary btn-sm">
-                    Direcciones
-                </button>
-            </div>
-        </li>
-        <li class="list-group-item list-group-item-action">
-            <h5>Nombre del lugar</h5>
-            <p>In adipisicing dolor magna enim ex nulla in ad amet.</p>
+    <div v-if="isLoadingPlaces" class="alert alert-primary text-center">
+        <h5>Cargando</h5>
+        <span>Wait please...</span>
+    </div>
+    <ul v-else-if="places.length > 0" class="list-group mt-3">
+        <li v-for="place in places" :key="place.id" class="list-group-item list-group-item-action">
+            <h5>{{place.text}}</h5>
+            <p>{{place.place_name}}</p>
             <div align="right">
                 <button class="btn btn-outline-primary btn-sm">
                     Direcciones
